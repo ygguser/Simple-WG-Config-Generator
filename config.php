8cd4dd96c0ad9aedd70046da2e6ae0d7469d6759
ygguser <86843390+ygguser@users.noreply.github.com>
2025-10-27 10:20:27 +0300

    added 'conf_wg_conf_path' variable; variable names with default values have been changed

--- config.php
+++ config.php
@@ -7,24 +7,42 @@ if ( realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME']) ) {
     exit;
 }
 
-// default values
-$server_public_address = 'myserver.com';
-$server_listen_port = '51820';
-$server_peer_ip = '10.0.0.1/24';
-$server_private_key = '';
-$server_public_key = '';
-$server_interface_name = 'ens3';
-$server_post_up = "iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $server_interface_name -j MASQUERADE";
-$server_post_down = "iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $server_interface_name -j MASQUERADE";
 
-$client_peer_ip = '10.0.0.2/32';
-$client_dns = '94.140.14.14, 94.140.15.15'; //adguard-dns.io/en/public-dns.html
-$client_post_up = '';
-$client_post_down = '';
-$client_allowed_ips = '0.0.0.0/0';
-$client_keep_alive = '30';
+//The path to the wireguard configuration file on the server.
+//For an example: $wg_conf_path = '/etc/wireguard/wg0.conf';
+//It is used to get data about the peers that are already in the configuration file (no changes are made to it by these scripts).
+//If empty, information about the current peers will simply not be displayed.
+//
+//Attention! In order for this to work, you also need to give the web server rights to read the /etc/wireguard directory and specifically the configuration file.
+//The sequence of commands for this:
+//chgrp www-data /etc/wireguard
+//chmod g+x /etc/wireguard
+//chgrp www-data /etc/wireguard/wg0.conf
+//chmod g+r /etc/wireguard/wg0.conf
+//
+//You also need to allow php to read a file from this directory by changing the value of the open_basedir variable in the php.ini configuration file (if you previously enabled this parameter).
+//For an example: open_basedir = /var/www/wgconf/:/etc/wireguard/:/var/log/:/tmp/
+$conf_wg_conf_path = '/etc/wireguard/wg0.conf';
+
+// default values
+$conf_server_public_address = 'myserver.com';
+$conf_server_listen_port = '51820';
+$conf_server_peer_ip = '10.0.0.1/24';
+$conf_server_private_key = '';
+$conf_server_public_key = '';
+$conf_server_interface_name = 'ens3';
+$conf_server_post_up = "iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $server_interface_name -j MASQUERADE";
+$conf_server_post_down = "iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $server_interface_name -j MASQUERADE";
+
+$conf_client_peer_ip = '10.0.0.2/32';
+$conf_client_dns = '94.140.14.14, 94.140.15.15'; //adguard-dns.io/en/public-dns.html
+$conf_client_post_up = '';
+$conf_client_post_down = '';
+$conf_client_allowed_ips = '0.0.0.0/0';
+$conf_client_keep_alive = '30';
 
 ?>
+
 
 
 
